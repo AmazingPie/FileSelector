12b0a8f7596db6c5477aa710457705e0113ceeff
AmazingPie <cjrulesgotit@gmail.com>
2019-06-01 18:21:00 +0100

    Fixed linking between scene and controller

--- src/FileSelector.java
+++ src/FileSelector.java
@@ -9,23 +9,35 @@ import javafx.stage.*;
 import javafx.scene.*;
 import javafx.fxml.*;
 
+import java.net.URL;
+import controllers.MainSceneController;
+
 /**
  *
  * @author chris
  */
 public class FileSelector extends Application
 {
+	private final URL main_scene;
+	
+	public FileSelector() {
+		main_scene = getClass().getResource("scenes/main_scene.fxml");
+	}
+	
 	public static void main(String args[]){    
 		launch(args);
 	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("main_scene.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("scenes/main_scene.fxml"));
+		loader.setLocation(main_scene);
+		loader.setController(new MainSceneController(primaryStage));
+		Parent root = (Parent)loader.load();
+		//MainSceneController controller = (MainSceneController)loader.getController();
 		Scene scene = new Scene(root, 600, 300);
 		primaryStage.setTitle("File Selector");
 		primaryStage.setScene(scene);
-		primaryStage.show();
-		
+		primaryStage.show(); 		
 	}
 }
--- src/controllers/MainSceneController.java
+++ src/controllers/MainSceneController.java
@@ -8,22 +8,58 @@ package controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.*;
+import javafx.scene.control.*;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import java.io.File;
 
 /**
- * FXML Controller class
+ * FXML Controller class for the main scene of the program.
+ * This will handle almost all of the buttons for controlling the program
  *
  * @author chris
  */
 public class MainSceneController implements Initializable
 {
-
+	@FXML private ImageView img_preview;
+	@FXML private TextField chosen_dir;
+	private Stage primaryStage;
+	private URL url;
+	private ResourceBundle rb;
+	
+	public MainSceneController(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+	}
+	
 	/**
 	 * Initializes the controller class.
 	 */
 	@Override
-	public void initialize(URL url, ResourceBundle rb)
-	{
-		// TODO
-	}	
+	public void initialize(URL url, ResourceBundle rb) {
+		this.url = url;
+		this.rb = rb;
+	}
+	
+	/**
+	 * Opens a directory chooser interface and sets the chosen_dir text field to the selected folder
+	 * @param e the event that was generated when button was pressed
+	 */
+	@FXML
+	protected void handleChooseDir(ActionEvent e) {
+		DirectoryChooser dc = new DirectoryChooser();
+		File chosen_file = dc.showDialog(primaryStage);
+	}
+	
+	/**
+	 * Generates a random image that is then displayed in the image preview
+	 * @param e the event that was generated when button was pressed
+	 */
+	@FXML 
+	protected void handleRandImg(ActionEvent e) {
+		
+	}
 	
 }
